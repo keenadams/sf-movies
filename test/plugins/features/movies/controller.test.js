13c4733b05122c93a5fd8c894714d757f8981637
Kienan Adams <kra8ff@virginia.edu>
2016-06-01 11:40:02 -0700

    feat(list): create list endpoint to list movies with filter options

--- test/plugins/features/movies/controller.test.js
+++ test/plugins/features/movies/controller.test.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Controller = require('../../../../lib/plugins/features/movies/controller');
+const Knex       = require('../../../../lib/libraries/knex');
 const Movie      = require('../../../../lib/models/movie');
 
 describe('movie controller', () => {
@@ -18,6 +19,47 @@ describe('movie controller', () => {
       })
       .then((movie) => {
         expect(movie.get('title')).to.eql(payload.title);
+      });
+    });
+
+  });
+
+  describe('list', () => {
+
+    beforeEach(() => {
+      const movieOne = { title: 'Batman Begins', release_year: 2005 };
+      const movieTwo = { title: 'The Dark Knight', release_year: 2008 };
+      const movieThree = { title: 'The Dark Knight Rises', release_year: 2012 };
+
+      return Knex('movies').insert([movieOne, movieTwo, movieThree]);
+    });
+
+    it('lists all movies', () => {
+      const query = {};
+
+      return Controller.list(query)
+      .then((movies) => {
+        expect(movies).to.have.lengthOf(3);
+      });
+    });
+
+    it('filters by title', () => {
+      const query = { title: 'the dark knight' };
+
+      return Controller.list(query)
+      .then((movies) => {
+        expect(movies).to.have.lengthOf(1);
+        expect(movies.models[0].get('title')).to.eql('The Dark Knight');
+      });
+    });
+
+    it('filters by release_year', () => {
+      const query = { release_year: 2012 };
+
+      return Controller.list(query)
+      .then((movies) => {
+        expect(movies).to.have.lengthOf(1);
+        expect(movies.models[0].get('release_year')).to.eql(2012);
       });
     });
 
--- test/plugins/features/movies/index.test.js
+++ test/plugins/features/movies/index.test.js
@@ -1,10 +1,20 @@
 'use strict';
 
+const Knex = require('../../../../lib/libraries/knex');
+
 const Movies = require('../../../../lib/server');
 
 describe('movies integration', () => {
 
   describe('create', () => {
+
+    beforeEach(() => {
+      const movieOne = { title: 'Batman Begins', release_year: 2005 };
+      const movieTwo = { title: 'The Dark Knight', release_year: 2008 };
+      const movieThree = { title: 'The Dark Knight Rises', release_year: 2012 };
+
+      return Knex('movies').insert([movieOne, movieTwo, movieThree]);
+    });
 
     it('creates a movie', () => {
       return Movies.inject({
@@ -15,6 +25,17 @@ describe('movies integration', () => {
       .then((response) => {
         expect(response.statusCode).to.eql(200);
         expect(response.result.object).to.eql('movie');
+      });
+    });
+
+    it('lists movies', () => {
+      return Movies.inject({
+        url: '/movies',
+        method: 'GET'
+      })
+      .then((response) => {
+        expect(response.statusCode).to.eql(200);
+        expect(response.result.data).to.have.lengthOf(3);
       });
     });
 
